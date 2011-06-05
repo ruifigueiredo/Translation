f63bb31025b93c0b087db0a750a70860d219b79a
Rui Figueiredo <rui.figueiredo@gmail.com>
2011-06-05 19:13:57 +0100

    Modulo Calendar

---
+++ vtiger/5.2.1/modules/Calendar/language/pt_pt.lang.php
@@ -0,0 +1,442 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific apmt_locationuage governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header$
+ * Description:  Defines the Portuguese Language Pack
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): Rui Figueiredo - ( rui.figueiredo@gmail.com )
+ ********************************************************************************/
+
+$mod_strings = Array(
+'LBL_MODULE_NAME'=>'Calendário',
+'LBL_MODULE_TITLE'=>'Calendário: Início',
+'LBL_MODULE_APPOINTMENT'=>'Calendário: Evento',
+'LBL_MODULE_APPOINTMENT_DETAIL'=>'Calendário: Detalhes do Evento',
+'LBL_SAVE'=>'Guardar',
+'LBL_RESET'=>'Cancelar',
+'LBL_LIST_USER_NAME'=>'Nome de Utilizador',
+'LBL_LIST_NAME'=>'Nome',
+'LBL_CALENDAR_SHARED'=>'Calendario partilhado com',
+'LBL_CALENDAR_SHARING'=>'Partilhar Calendario com',
+'LBL_SHARING_OPTION'=>'O calendario está visivel?',
+'LBL_LIST_TOOLS'=>'Ferramentas',
+'LBL_SETTINGS'=>'Configurações',
+'LBL_CALSETTINGS'=>'Configurações do Calendário',
+'LBL_USE24'=>'Usar formato 24 horas',
+'LBL_CALSTART'=>'Começar o meu calendário as ',
+'LBL_TIMESETTINGS'=>'Configurações da Hora',
+'LBL_HOLDFOLLOWUP'=>'Hold followup on',
+'LBL_CALL' => 'Chamada',
+'LBL_MEET' => 'Reunião',
+'LBL_APPNT'=>'Compromisso',
+'LBL_NEW_APPNT'=>'Novo Compromisso',
+'LBL_NEW_APPNT_INFO'=>'Criar um Novo Compromisso',
+'LBL_VIEW_DAY_APPNT_INFO'=>'Ver o(s) Compromisso(s) de Hoje',
+'LBL_CHANGE_APPNT'=>'Ver/Alterar este Compromisso (%s - %s)',
+
+'LBL_DAY' => 'Dia',
+'LBL_DAY_BUTTON_KEY' => 'D',
+'LBL_DAY_BUTTON_TITLE' => 'Dia [Alt+D]',
+'LBL_DAY1' => 'Segunda',
+'LBL_DAY2' => 'Terça',
+'LBL_DAY3' => 'Quarta',
+'LBL_DAY4' => 'Quinta',
+'LBL_DAY5' => 'Sexta',
+'LBL_DAY6' => 'Sabado',
+'LBL_DAY0' => 'Domingo',
+
+'LBL_SM_MON' => 'Seg',
+'LBL_SM_TUE' => 'Ter',
+'LBL_SM_WED' => 'Qua',
+'LBL_SM_THU' => 'Qui',
+'LBL_SM_FRI' => 'Sex',
+'LBL_SM_SAT' => 'Sab',
+'LBL_SM_SUN' => 'Dom',
+
+'LBL_DATE_TITLE' => 'Dia %d, %A',
+
+'LBL_WEEK' => 'Semana',
+'LBL_WEEK_BUTTON_KEY' => 'W',
+'LBL_WEEK_BUTTON_TITLE' => 'Semana [Alt+W]',
+'LBL_WEEKS' => 'Semanas',
+'LBL_NEXT_WEEK' => 'próxima semana',
+'LBL_LAST_WEEK' => 'última semana',
+'LBL_4WEEKS_BACK'=>'Últimas 4 Semanas',
+'LBL_4WEEKS_PLUS'=>'Proximas 4 Semanas',
+'LBL_RELOAD' => 'Actualizar',
+
+
+'LBL_APPCREATED_BY' => 'Criado por',
+'LBL_AT_DATE_TIME' => 'em',
+
+'LBL_MON' => 'Mês',
+'LBL_MON_BUTTON_KEY' => 'M',
+'LBL_MON_BUTTON_TITLE' => 'Mês [Alt+M]',
+'LBL_PREV_MON' => 'Mês Anterior',
+'LBL_NEXT_MON' => 'Mês Seguinte',
+
+'LBL_YEAR_BUTTON_KEY'=>'Y',
+'LBL_PREV_YEAR'=>'Ano Anterior',
+'LBL_NEXT_YEAR' => 'Ano Seguinte',
+
+'LBL_APP_LOCATION' => 'Localização',
+'LBL_APP_IGNORE_TIME' => 'Ignorar horas acima',
+'LBL_SUBJECT'=>'Assunto:',
+'LBL_APP_DESCRIPTION' => 'Descrição',
+'LBL_CONTACT'=>'Contacto:',
+'LBL_CONTACT_LIST'=>'Lista de Contactos:',
+
+'LBL_APP_IGNORE_TIME2' => '(i.e. o compromisso ocorre<br />em nenhuma hora particular neste(s) dia(s) )',
+
+'LBL_APP_ERR001' =>'Data inválida no campo %s!',
+'LBL_APP_ERR002' =>'Íniciar após o final !',
+'LBL_APP_ERR003' =>'Sem Contacto !',
+'LBL_APP_ERR004' =>'Sem Assunto !',
+'ERR_DELETE_RECORD'=>"Indique um numero de registo para apagar o Compromisso.",
+'DELETE_CONFIRMATION'=>"Tem a certeza que deseja apagar este Compromisso ?",
+
+'AppLoc'=> Array('0' => 'Escritório'
+			, '1' => 'Ausente'
+			, '2' => 'Férias'
+			, '3' => 'Doente'
+			, '4' => 'Adiadi'
+			, '5' => 'Opção'
+			, '6' => 'Privado'),
+
+'cal_month_long'=>array(
+"",
+"Janeiro",
+"Fevereiro",
+"Março",
+"Abril",
+"Maio",
+"Junho",
+"Julho",
+"Agosto",
+"Setembro",
+"Outubro",
+"Novembro",
+"Dezembro",
+),
+
+'cal_weekdays_short'=>array(
+"Dom",
+"Seg",
+"Ter",
+"Qua",
+"Qui",
+"Sex",
+"Sab",
+),
+'cal_weekdays_long'=>array(
+"Domingo",
+"Segunda",
+"Terça",
+"Quarta",
+"Quinta",
+"Sexta",
+"Sabado",
+),
+'cal_month_short'=>array(
+"",
+"Jan",
+"Fev",
+"Mar",
+"Abr",
+"Mai",
+"Jun",
+"Jul",
+"Ago",
+"Set",
+"Out",
+"Nov",
+"Dez",
+),
+
+'LBL_TIME'=>'Hora',
+'LBL_START_TIME' => 'Hora de Começo',
+'LBL_END_TIME' => 'Hora de Fim',
+'LBL_START_DATE'=>'Data de Começo',
+'LBL_TIME_START'=>'Hora de Começo',
+'LBL_DUE_DATE'=>'Data Limite',
+'LBL_START_DATE_TIME'=>'Data & Hora de Começo',
+'LBL_END_DATE_TIME'=>'Data & Hora de Fim',
+'LBL_TODO'=>'To Do',
+'LBL_TODOS'=>'To Dos',
+'LBL_EVENTS'=>'Eventos',
+'LBL_TOTALEVENTS'=>'Os Meus Eventos :',
+'LBL_TOTALTODOS'=>'Os Meus To Dos :',
+'LBL_VIEW'=>'View',
+'LBL_LISTVIEW'=>'List View',
+'LBL_HRVIEW'=>'Hour View',
+'LBL_WEEKVIEW'=>'Vista Semanal do Calendário',
+'LBL_MONTHVIEW'=>'Vista Mensal do Calendário',
+'LBL_YEARVIEW'=>'Vista Anual do Calendário',
+'LBL_STATUS'=>'Estado',
+'LBL_ACTION'=>'Acções',
+'LBL_ADD'=>'Adicionar',
+'LBL_OPENCAL'=>'Abrir Calendario',
+
+
+'LBL_ADD_EVENT'=>'Adicionar Evento',
+'LBL_ADDCALL'=>'Chamada',
+'LBL_ADDMEETING'=>'Reunião',
+'LBL_ADDTODO'=>'To Do',
+'LBL_BEFOREEVENT'=>'antes do evento começar',
+'LBL_BEFORETASK'=>'antes da tarefa começar',
+'LBL_EVENTDETAILS'=>'Detalhes do Evento',
+'LBL_CURSTATUS'=>'Estado Actual',
+'LBL_ASSINGEDTO'=>'Atribuido a',
+'LBL_RELATEDTO'=>'Relacionado Com',
+'LBL_PENDING'=>'Pendente',
+'LBL_PUBLIC'=>'Marcar Público',
+'LBL_MORE'=>'Mais',
+'LBL_EDIT'=>'Editar',
+'LBL_EVERYDAY'=>'Todos os Dias',
+'LBL_EVERYWEEK'=>'Todas as Semanas',
+'LBL_EVERYMON'=>'Todos os Mêses',
+'LBL_WEEKS'=>'Semana(s)',
+'LBL_MONTHS'=>'Mês(es)',
+'LBL_YEAR'=>'Ano',
+'LBL_NONE_SCHEDULED'=>'None Scheduled',
+
+'LBL_INVITE_INST1'=>'Para convidar, seleccione os utilizadores da lista "Utilizadores Disponíveis" e carregue no botão "Adicionar".',
+'LBL_INVITE_SHARE'=>'Para partilhar, seleccione os utilizadores da lista "Utilizadores Disponíveis" e carregue no botão "Adicionar".',
+'LBL_INVITE_INST2'=>'Para remover, seleccione os utilizadores da lista "Ulizadores Seleccionados" e carregue no botão "Remover".',
+'LBL_SELUSR_INFO'=>' Selected users will receive an email about the Event.',
+'LBL_CALSHAREMESSAGE'=>'Partilhar o meu calendário com os seguintes utilizadores',
+'LBL_CALSHARE'=>'Partilha do Calendário',
+'LBL_SEL_USERS'=>'Utilizadores Seleccionados',
+'LBL_AVL_USERS'=>'Utilizadores Disponiveis',
+'LBL_ADD_BUTTON'=>'Adicionar',
+'LBL_USERS'=>'Utilizadores',
+'LBL_RMV_BUTTON'=>'Remover',
+'LBL_SDRMD'=>'Enviar Lembrete para',
+'LBL_ENABLE_REPEAT'=>'Activar Repetição',
+'LBL_REPEAT_ONCE'=>'Repetir uma vez cada',
+'LBL_ADD_TODO'=>'Criar To Do',
+'LBL_TODONAME'=>'To Do',
+'LBL_TODODATETIME'=>'Hora & Data',
+
+
+
+
+
+//DON'T CONVERT THESE THEY ARE MAPPINGS - STARTS
+'db_last_name' => 'LBL_LIST_LAST_NAME',
+'db_first_name' => 'LBL_LIST_FIRST_NAME',
+'db_title' => 'LBL_LIST_TITLE',
+'db_email1' => 'LBL_LIST_EMAIL_ADDRESS',
+'db_email2' => 'LBL_LIST_EMAIL_ADDRESS',
+//DON'T CONVERT THESE THEY ARE MAPPINGS -ENDS
+'LBL_COMPLETED'=>'Marcar como Completo',
+'LBL_DEFERRED'=>'Marcar como Adiado',
+'LBL_HELD'=>'Marcar como Realizado',
+'LBL_NOTHELD'=>'Marcar como Não Realizado',
+'LBL_POSTPONE'=>'Adiado',
+'LBL_CHANGEOWNER'=>'Mudar Dono',
+'LBL_DEL'=>'Apagar',
+
+//Added for actvity merge with calendar
+'LBL_SEARCH_FORM_TITLE'=>'Pesquisa de Actividade',
+'LBL_LIST_FORM_TITLE'=>'Lista de Actividades',
+'LBL_NEW_FORM_TITLE'=>'Nova Actividade',
+'LBL_TASK_INFORMATION'=>'Informação da Tarefa',
+'LBL_EVENT_INFORMATION'=>'Informação do Evento',
+'LBL_CALENDAR_INFORMATION'=>'Informação do Calendário',
+
+'LBL_NAME'=>'Assunto:',
+'LBL_ACTIVITY_NOTIFICATION'=>'This is a notification that an activity is assigned to you that has been',
+'LBL_ACTIVITY_INVITATION'=>'You have been invited for an activity that has been ',
+'LBL_DETAILS_STRING'=>'Os detalhes são',
+'LBL_REGARDS_STRING'=>'Obrigado',
+'LBL_CONTACT_NAME'=>'Nome do Contacto',
+'LBL_OPEN_ACTIVITIES'=>'Abrir Actividades',
+'LBL_ACTIVITY'=>'Actividade:',
+'LBL_HISTORY'=>'Histórico',
+'LBL_UPCOMING'=>"As Minhas Actividades Pendentes ou Para Breve",
+'LBL_TODAY'=>'through ',
+
+'LBL_NEW_TASK_BUTTON_TITLE'=>'Nova Tarefa [Alt+N]',
+'LBL_NEW_TASK_BUTTON_KEY'=>'N',
+'LBL_NEW_TASK_BUTTON_LABEL'=>'Nova Tarefa',
+'LBL_SCHEDULE_MEETING_BUTTON_TITLE'=>'Marcar Reunião [Alt+M]',
+'LBL_SCHEDULE_MEETING_BUTTON_KEY'=>'M',
+'LBL_SCHEDULE_MEETING_BUTTON_LABEL'=>'Marcar Reunião',
+'LBL_SCHEDULE_CALL_BUTTON_TITLE'=>'Marcar Telefonema [Alt+C]',
+'LBL_SCHEDULE_CALL_BUTTON_KEY'=>'C',
+'LBL_SCHEDULE_CALL_BUTTON_LABEL'=>'Marcar Telefonema',
+'LBL_NEW_NOTE_BUTTON_TITLE'=>'Novo Documento [Alt+T]',
+'LBL_NEW_ATTACH_BUTTON_TITLE'=>'Anexar Ficheiro [Alt+F]',
+'LBL_NEW_NOTE_BUTTON_KEY'=>'T',
+'LBL_NEW_ATTACH_BUTTON_KEY'=>'F',
+'LBL_NEW_NOTE_BUTTON_LABEL'=>'Novo Documento',
+'LBL_NEW_ATTACH_BUTTON_LABEL'=>'Anexar Ficheiro',
+'LBL_TRACK_EMAIL_BUTTON_TITLE'=>'Track Email [Alt+K]',
+'LBL_TRACK_EMAIL_BUTTON_KEY'=>'K',
+'LBL_TRACK_EMAIL_BUTTON_LABEL'=>'Track Email',
+
+'LBL_LIST_CLOSE'=>'Fechar',
+'LBL_LIST_STATUS'=>'Estado',
+'LBL_LIST_CONTACT'=>'Contacto',
+//Added for 4.2 release for Account column support as shown by Fredy
+'LBL_LIST_ACCOUNT'=>'Conta',
+'LBL_LIST_RELATED_TO'=>'Relacionado Com',
+'LBL_LIST_DUE_DATE'=>'Data Limite',
+'LBL_LIST_DATE'=>'Data',
+'LBL_LIST_SUBJECT'=>'Assunto',
+'LBL_LIST_LAST_MODIFIED'=>'Última Actualização',
+'LBL_LIST_RECURRING_TYPE'=>'Recurring Type',
+
+'ERR_DELETE_RECORD'=>"A record number must be specified to delete the vtiger_account.",
+'NTC_NONE_SCHEDULED'=>'None scheduled.',
+
+// Added vtiger_fields for Attachments in Activities/SubPanelView.php
+'LBL_ATTACHMENTS'=>'Anexos',
+'LBL_NEW_ATTACHMENT'=>'Novo Anexo',
+
+//Added vtiger_fields after RC1 - Release
+'LBL_ALL'=>'Todos',
+'LBL_CALL'=>'Telefonema',
+'LBL_MEETING'=>'Reunião',
+'LBL_TASK'=>'Tarefa',
+
+//Added for 4GA Release
+'Subject'=>'Assunto',
+'Assigned To'=>'Atribuido a',
+'Start Date & Time'=>'Hora e Data de Início',
+'Time Start'=>'Data de Início',
+'Due Date'=>'Data Limite',
+'Related To'=>'Relacionado Com',
+'Contact Name'=>'Nome do Contacto',
+'Status'=>'Estado',
+'Priority'=>'Prioridade',
+'Visibility'=>'Visibilidade',
+'Send Notification'=>'Enviar Notificação',
+'Created Time'=>'Data de Criação',
+'Modified Time'=>'Data de Actualização',
+'Activity Type'=>'Tipo de Actividade',
+'Description'=>'Descrição',
+'Duration'=>'Duração',
+'Duration Minutes'=>'Duração em Minutos',
+'Location'=>'Localização',
+'No Time'=>'Sem tempo',
+//Added for Send Reminder 4.2 release
+'Send Reminder'=>'Enviar Lembrete',
+'LBL_YES'=>'Sim',
+'LBL_NO'=>'Não',
+'LBL_DAYS'=>'dia(s)',
+'LBL_MINUTES'=>'minutos',
+'LBL_HOURS'=>'horas',
+'LBL_BEFORE_EVENT'=>'Antes do evento',
+//Added for CustomView 4.2 Release
+'Close'=>'Fechar',
+'Start Date'=>'Data de Ínicio',
+'Type'=>'Tipo',
+'End Date'=>'Data de Fim',
+'Recurrence'=> 'Recurring Events',
+'Recurring Type'=> 'Recurring Type',
+//Activities - Notification Error
+'LBL_NOTIFICATION_ERROR'=>'Mail Error : Please check your outgoing mail server configuration in Settings->Outgoing Mail Server Configuration OR MailId for the present user is not Configured',
+// Mike Crowe Mod --------------------------------------------------------added for generic search
+'LBL_GENERAL_INFORMATION'=>'Informação Geral',
+
+'LBL_EVENTTYPE'=>'Tipo de Evento',
+'LBL_EVENTNAME'=>'Nome do Evento',
+'LBL_EVENTSTAT'=>'Hora de Inicio do Evento',
+'LBL_EVENTEDAT'=>'Evento Termina as',
+'LBL_INVITE'=>'Convidar',
+'LBL_REPEAT'=>'Repetir',
+'LBL_REMINDER'=>'Lembrete',
+'LBL_SENDREMINDER'=>'Enviar Lembrete',
+'LBL_NOTIFICATION'=>'Notificação',
+'LBL_SENDNOTIFICATION'=>'Enviar Notificação',
+'LBL_RMD_ON'=>'Lembrar em',
+'LBL_REPEATEVENT'=>'Repeat once in every',
+'LBL_TIMEDATE'=>'Hora & Data',
+'LBL_HR'=>'Hr',
+'LBL_MIN'=>'min',
+'LBL_EVENT'=>'Evento',
+'Daily'=>'Dia(s)',
+'Weekly'=>'Semana(s)',
+'Monthly'=>'Mês(s)',
+'Yearly'=>'Ano',
+'createdtime'=>'Criado a',
+'modifiedtime'=>'Actualizado a',
+'first'=>'Primeiro',
+'last'=>'Útlimo',
+'High'=>'Alto',
+'Medium'=>'Médio',
+'Low'=>'Baixo',
+'LBL_SELECT'=>'Seleccionar',
+'LBL_ALL_EVENTS_TODOS'=>'Todos os Eventos & To Dos',
+'First'=>'Primeiro', 
+'Last'=>'Último', 
+'on'=>'em', 
+'day of the month'=>'dia do mês',
+'Private'=>'Privado',
+'Public'=>'Público',
+
+//Added for existing Picklist entries
+
+'Planned'=>'Planeado',
+'Held'=>'Held',
+'Not Held'=>'Not Held',
+'Completed'=>'Completo',
+'Deferred'=>'Adiado',
+'Not Started'=>'Não começado',
+'In Progress'=>'Em Progresso',
+'Pending Input'=>'A espera de informação',
+'LBL_REMAINDER_DAY'=>'dias',
+'LBL_REMAINDER_HRS'=>'horas',
+'Call'=>'Telefonema',
+'Meeting'=>'Reunião',
+
+//added to send dates and time in calendar notification/invitation mail.
+
+'Start date and time'=>'Data & Hora de Início',
+'End date and time'=>'Data & Hora de Fim',
+//this is for task
+'End date'=>'Data de Fim',
+'LBL_SET_DATE'=>'Definir data..',
+'Recurrence'=>'Recurrence',
+
+//added to send invitation mail Subject.
+'INVITATION'=>' Convite ',
+
+// Added/Updated for vtiger CRM 5.0.4
+'LBL_YEAR_BUTTON_TITLE'=>'Ano [Alt+Y]',
+'LBL_SELECT_CONTACT'=>'Seleccione Contactos',
+'SHARED_EVENT_DEL_MSG'=>'O Utilizador não tem permissões para Editar/Apagar o Evento Partilhado.',
+//added to fix ticket#4525
+'LBL_CREATED'=>'criado',
+'LBL_UPDATED'=>'actualizado',
+
+//Added after 5.0.4 GA
+'LBL_BUSY' => 'Ocupado',
+
+//Custom Fields support for Calendar
+'LBL_CUSTOM_INFORMATION'=>'Informação Específica',
+
+// Repeat Event support for Calendar
+'LBL_UNTIL' => 'Até',
+'LBL_SET_DATE'=>'Set Date',
+
+'LBL_MINE' =>'Meu',
+
+);
+
+?>
