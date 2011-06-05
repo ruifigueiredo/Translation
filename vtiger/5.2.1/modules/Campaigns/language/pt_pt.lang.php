87579eb25ec5d3b21ca9e94e3d238c90830d2759
Rui Figueiredo <rui.figueiredo@gmail.com>
2011-06-05 19:20:13 +0100

    Modulo Campaigns

---
+++ vtiger/5.2.1/modules/Campaigns/language/pt_pt.lang.php
@@ -0,0 +1,91 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header$
+ * Description:  Defines the Portuguese Language Pack
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): Rui Figueiredo - ( rui.figueiredo@gmail.com )
+ ********************************************************************************/
+
+$mod_strings = Array(
+// Added in release 5.0
+'LBL_CAMPAIGN_INFORMATION'=>'Informações da Campanha',
+'LBL_CUSTOM_INFORMATION'=>'Informação personalizada',
+'LBL_DESCRIPTION_INFORMATION'=>'Informação Descritiva',
+'LBL_EXPECTATIONS_AND_ACTUALS'=>'Expectativas & Actuais',
+
+'Campaign Name'=>'Nome da Campanha',
+'Campaign Type'=>'Tipo de Campanha',
+'Campaign Status'=>'Estado da Campanha',
+'Campaign ID'=>'ID da Campanha',
+'Expected Close Date'=>'Data de Fecho Esperada',
+'Expected Revenue'=>'Expected Revenue',
+'Budget Cost'=>'Orçamento',
+'Actual Cost'=>'Custo Actual',
+'Expected Response'=>'Resposta Esperada',
+'Assigned To'=>'Atribuido a',
+'Num Sent'=>'Num Sent',
+'Created Time'=>'Data de Criação',
+'Modified Time'=>'Data de Alteração',
+'Description'=>'Descrição',
+'Product'=>'Produto',
+'Target Audience'=>'Audiência Alvo',
+'TargetSize'=>'Tamanho do Alvo',
+'Sponsor'=>'Patrocinador',
+
+'Expected Sales Count'=>'Expected Sales Count',
+'Expected Response Count'=>'Expected Response Count',
+'Expected ROI'=>'Expected ROI',
+
+'Actual Sales Count'=>'Actual Sales Count',
+'Actual Response Count'=>'Actual Response Count',
+'Actual ROI'=>'Actual ROI',
+'Select One'=>'Select One',
+'LBL_LOAD_LIST'=>'Load List',
+
+//Added for existing Picklist Entries
+
+'Conference'=>'Conferencia',
+'Webinar'=>'Webinar',
+'Trade Show'=>'Trade Show',				    
+'Public Relations'=>'Relações Públicas',
+'Partners'=>'Parceiros',
+'Referral Program'=>'Referral Program',
+'Advertisement'=>'Publicidade',
+'Banner Ads'=>'Banner Ads',
+'Direct Mail'=>'Direct Mail',
+'Email'=>'Email',
+'Telemarketing'=>'Telemarketing',
+'Others'=>'Outros',
+
+'Planning'=>'Planeamento',						      	    
+'Active'=>'Activo',	
+'Inactive'=>'Inactivo',
+'Complete'=>'Completo',
+'Cancelled'=>'Cancelado',							      
+
+'Excellent'=>'Excelente',
+'Good'=>'Bom',
+'Average'=>'Mediano',
+'Poor'=>'Fraco',
+			      
+//Module Sequence Numbering
+'Campaign No'=>'N. da Campanha',
+// END
+
+);
+
+?>
