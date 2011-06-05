9e36feb543c252b048909de00eab7de4c2cb8505
Rui Figueiredo <rui.figueiredo@gmail.com>
2011-06-05 13:00:09 +0100

    Modulo Accounts

---
+++ vtiger/5.2.1/modules/Accounts/language/pt_pt.lang.php
@@ -0,0 +1,242 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header$
+ * Description:  Defines the Portuguese Language Pack
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): Rui Figueiredo ( rui.figueiredo@gmail.com )
+ ********************************************************************************/
+ 
+$mod_strings = Array(
+'LBL_MODULE_NAME'=>'Contas',
+'LBL_MODULE_TITLE'=>'Contas: Início',
+'LBL_SEARCH_FORM_TITLE'=>'Pesquisa de Contas',
+'LBL_LIST_FORM_TITLE'=>'Lista de Contas',
+'LBL_NEW_FORM_TITLE'=>'Nova Conta',
+'LBL_MEMBER_ORG_FORM_TITLE'=>'Organizações do Membro',
+// Label for Top Accounts in Home Page, added for 4.2 GA
+'LBL_TOP_ACCOUNTS'=>'As Minhas Contas Principais', 
+'LBL_TOP_AMOUNT'=>'Quantia',
+'LBL_LIST_ACCOUNT_NAME'=>'Nome da Conta',
+'LBL_LIST_CITY'=>'Cidade',
+'LBL_LIST_WEBSITE'=>'Website',
+'LBL_LIST_STATE'=>'Localidade',
+'LBL_LIST_PHONE'=>'Telefone',
+'LBL_LIST_EMAIL_ADDRESS'=>'Email',
+'LBL_LIST_CONTACT_NAME'=>'Nome do Contacto',
+'LBL_LIST_AMOUNT' => 'Quantia Total',
+
+//DON'T CONVERT THESE THEY ARE MAPPINGS
+'db_name' => 'LBL_LIST_ACCOUNT_NAME',
+'db_website' => 'LBL_LIST_WEBSITE',
+'db_billing_address_city' => 'LBL_LIST_CITY',
+
+//END DON'T CONVERT
+
+'LBL_ACCOUNT'=>'Conta:',
+'LBL_ACCOUNT_NAME'=>'Nome da Conta:',
+'LBL_PHONE'=>'Telefone:',
+'LBL_WEBSITE'=>'Website:',
+'LBL_FAX'=>'Fax:',
+'LBL_TICKER_SYMBOL'=>'Símbolo na Bolsa:',
+'LBL_OTHER_PHONE'=>'Outro Telefone:',
+'LBL_ANY_PHONE'=>'Qualquer Telefone:',
+'LBL_MEMBER_OF'=>'Membro de:',
+'LBL_EMAIL'=>'Email:',
+'LBL_EMPLOYEES'=>'Colaboradores:',
+'LBL_OTHER_EMAIL_ADDRESS'=>'Outro Email:',
+'LBL_ANY_EMAIL'=>'Qualquer Email:',
+'LBL_OWNERSHIP'=>'Ownership:',
+'LBL_RATING'=>'Avaliação:',
+'LBL_INDUSTRY'=>'Industria:',
+'LBL_SIC_CODE'=>'Classif. Activ. Economicas:',
+'LBL_TYPE'=>'Tipo:',
+'LBL_ANNUAL_REVENUE'=>'Receita Anual:',
+'LBL_ADDRESS_INFORMATION'=>'Informações da Morada',
+'LBL_ACCOUNT_INFORMATION'=>'Informação da Conta',
+'LBL_CUSTOM_INFORMATION'=>'Informação Específica',
+'LBL_BILLING_ADDRESS'=>'Morada de Facturação:',
+'LBL_SHIPPING_ADDRESS'=>'Morada de Envio:',
+'LBL_ANY_ADDRESS'=>'Qualquer Morada:',
+'LBL_CITY'=>'Cidade:',
+'LBL_STATE'=>'Localidade:',
+'LBL_POSTAL_CODE'=>'Código Postal:',
+'LBL_COUNTRY'=>'País:',
+'LBL_DESCRIPTION_INFORMATION'=>'Informação Descritiva',
+'LBL_DESCRIPTION'=>'Descrição:',
+'NTC_COPY_BILLING_ADDRESS'=>'Copiar morade de facturação para morada de entregas',
+'NTC_COPY_SHIPPING_ADDRESS'=>'Copiar morada de entregas para morada de facturação',
+'NTC_REMOVE_MEMBER_ORG_CONFIRMATION'=>'Are you sure you want to remove this record as a member organization?',
+'LBL_DUPLICATE'=>'Possibilidade de Duplicar Contas',
+'MSG_DUPLICATE' => 'Ao criar esta Conta, poderá duplica-la. Poderá também seleccionar uma Conta da lista abaixo ou seleccionar a opção Nova Conta para criar uma Conta com os dados da anterior.',
+
+'LBL_INVITEE'=>'Contactos',
+'ERR_DELETE_RECORD'=>"Indique um numero de registo para apagar a Conta.",
+
+'LBL_SELECT_ACCOUNT'=>'Seleccione Conta',
+'LBL_GENERAL_INFORMATION'=>'Informação Geral',
+
+//for v4 release added
+'LBL_NEW_POTENTIAL'=>'Nova Oportunidade',
+'LBL_POTENTIAL_TITLE'=>'Oportunidades',
+
+'LBL_NEW_TASK'=>'Nova Tarefa',
+'LBL_TASK_TITLE'=>'Tarefas',
+'LBL_NEW_CALL'=>'Novo Telefonema',
+'LBL_CALL_TITLE'=>'Telefonemas',
+'LBL_NEW_MEETING'=>'Nova Reunião',
+'LBL_MEETING_TITLE'=>'Reuniões',
+'LBL_NEW_EMAIL'=>'Novo Email',
+'LBL_EMAIL_TITLE'=>'Emails',
+'LBL_NEW_CONTACT'=>'Novo Contacto',
+'LBL_CONTACT_TITLE'=>'Contactos',
+
+//Added vtiger_fields after RC1 - Release
+'LBL_ALL'=>'Todos',
+'LBL_PROSPECT'=>'Potencial',
+'LBL_INVESTOR'=>'Investidor',
+'LBL_RESELLER'=>'Revendedor',
+'LBL_PARTNER'=>'Parceiro',
+
+// Added for 4GA
+'LBL_TOOL_FORM_TITLE'=>'Ferramentas de Contas',
+//Added for 4GA
+'Account Name'=>'Nome da Conta',
+'Phone'=>'Telefone',
+'Website'=>'Website',
+'Fax'=>'Fax',
+'Ticker Symbol'=>'Símbolo da Bolsa',
+'Other Phone'=>'Outro Telefone',
+'Member Of'=>'Membro de',
+'Email'=>'Email',
+'Employees'=>'Colaboradores',
+'Other Email'=>'Outros Emails',
+'Ownership'=>'Dono',
+'Rating'=>'Avaliação',
+'industry'=>'Industria',
+'SIC Code'=>'Classif. Activ. Economicas',
+'Type'=>'Tipo',
+'Annual Revenue'=>'Receita Anual',
+'Assigned To'=>'Responsável:',
+'Billing Address'=>'Morada de Facturação',
+'Shipping Address'=>'Morada para Entregas',
+'Billing City'=>'Cidade de Facturação',
+'Shipping City'=>'Cidade para Entregas',
+'Billing State'=>'Concelho de Facturação',
+'Shipping State'=>'Concelho para Entregas',
+'Billing Code'=>'Código Postal de Facturação',
+'Shipping Code'=>'Código Postal para Entregas',
+'Billing Country'=>'País de Facturação',
+'Shipping Country'=>'País para Entregas',
+'Created Time'=>'Data Criada',
+'Modified Time'=>'Data de Modificação',
+'Description'=>'Descrição',
+'Shipping Po Box'=>'Apartado para Entrega',
+'Billing Po Box'=>'Apartado para Facturação',
+
+//Added after 4.2 patch 2
+'Email Opt Out'=>'Recusa Email',
+'LBL_EMAIL_OPT_OUT'=>'Recusa Email:',
+
+//Added after 5Alpha5
+'Notify Owner'=>'Notificar Responsável',
+
+//Added for existing picklist entries
+
+'--None--'=>'--Nada--',
+
+'Acquired'=>'Adquirido',
+'Active'=>'Activo',
+'Market Failed'=>'Mercado Falhado',
+'Project Cancelled'=>'Projecto Cancelado',
+'Shutdown'=>'Terminado',
+
+'Apparel'=>'Vestuário',
+'Banking'=>'Bancos',
+'Biotechnology'=>'Bio-Tecnologia',
+'Chemicals'=>'Químicos',
+'Communications'=>'Comunicações',
+'Construction'=>'Construção',
+'Consulting'=>'Consultoria',
+'Education'=>'Educação',
+'Electronics'=>'Electrónicas',
+'Energy'=>'Energia',
+'Engineering'=>'Engenharia',
+'Entertainment'=>'Entretenimento',
+'Environmental'=>'Ambiente',
+'Finance'=>'Finanças',
+'Food & Beverage'=>'Food & Beverage',
+'Government'=>'Governo',
+'Healthcare'=>'Saúde',
+'Hospitality'=>'Hospitalidade',
+'Insurance'=>'Seguros',
+'Machinery'=>'Maquinaria',
+'Manufacturing'=>'Manufacturação',
+'Media'=>'Media',
+'Not For Profit'=>'Sem Fins Lucrativos',
+'Recreation'=>'Recreativos',
+'Retail'=>'Retail',
+'Shipping'=>'Entregas',
+'Technology'=>'Tecnologia',
+'Telecommunications'=>'Telecomunicações',
+'Transportation'=>'Transportes',
+'Utilities'=>'Utilities',
+'Other'=>'Outro',
+
+'Analyst'=>'Analista',
+'Competitor'=>'Concorrente',
+'Customer'=>'Cliente',
+'Integrator'=>'Integrador',
+'Investor'=>'Investidor',
+'Partner'=>'Parceiro',
+'Press'=>'Press',
+'Prospect'=>'Potencial',
+'Reseller'=>'Revendedor',
+'LBL_START_DATE' => 'Início',
+'LBL_END_DATE' => 'Fim',
+
+// Added/Updated for vtiger CRM 5.0.4
+
+//added to fix the issue #4081
+'LBL_ACCOUNT_EXIST' => 'Nome da Conta Já Existe!',
+
+// mailer export
+'LBL_MAILER_EXPORT' => 'Mailer Export',
+'LBL_MAILER_EXPORT_CONTACTS_TYPE'=>'Seleccionar Contactos:',
+'LBL_MAILER_EXPORT_CONTACTS_DESCR'=>'Contacts can also be selected via "Custom Fields" and some standard fields.',
+'LBL_MAILER_EXPORT_RESULTS_TYPE'=>'Seleccionar tipo de exportação:',
+'LBL_MAILER_EXPORT_RESULTS_DESCR'=>'The data will gathered from the Accounts and their Contacts, that were returned
+ from the previous search.',
+'LBL_EXPORT_RESULTS_EMAIL' => 'Export E-Mailer data',
+'LBL_EXPORT_RESULTS_EMAIL_CORP'=>'Export E-Mail data, using "Company Email" from the Account if the Contacts "Email" is empty',
+'LBL_EXPORT_RESULTS_FULL'=>'Export data with the Contacts, E-Mail, Account name, Address, Telephone, etc.',
+'LBL_EXPORT_RESULTS_GO'=>'Exportar',
+'LBL_MAILER_EXPORT_IGNORE' => '--ignorar--',
+'LBL_MAILER_EXPORT_CHECKED' =>'Verificado',
+'LBL_MAILER_EXPORT_NOTCHECKED' => 'Não Verificado',
+
+// Added after 5.0.4 GA
+
+//Module Sequence Numbering
+'Account No'=>'Conta Número.',
+// END
+
+// Account Hierarchy
+'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Mostar Hierarquia das Contas', 
+
+);
+
+?>
